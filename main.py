cd979c9da4b4a116c73bbf5baa9da28f475967e3
Bafin228 <kovalenko.andrey17@mail.ru>
2023-02-25 23:49:05 +0300

    dvisjenie poka chto chara

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+sc = pygame.display.set_mode((700, 700))
+clock = pygame.time.Clock()
 
+x = 200
+y = 100
+fat = 10
+FPS = 30
+pos = (x, y, fat, fat)
+game = True
+color = (0, 255, 0)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while game:
+    clock.tick(FPS)
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        x += 10
+    if keys[pygame.K_w]:
+        y -= 10
+    if keys[pygame.K_s]:
+        y += 10
+    if keys[pygame.K_a]:
+        x -= 10
+    pos = (x, y, fat, fat)
+    sc.fill((0, 0, 0))
+    pygame.draw.rect(sc, color, pos,)
+    pygame.display.update()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pygame.quit()
